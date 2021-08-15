845258749fc9c6305ebaf650dc5c23042a92859a
Pablorf10 <rafanet.live@gmail.com>
2021-08-15 19:43:33 -0300

    Adicionando google fonts

--- lib/LoginSignUpPage.dart
+++ lib/LoginSignUpPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginSignUpPage extends StatefulWidget {
   @override
@@ -9,9 +10,23 @@ class _LoginSignUpPageState extends State<LoginSignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      child: Container(
+      body: Container(
         child: SingleChildScrollView(
-          child: Column(),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 120, bottom: 22),
+                child: Center(
+                  child: Text(
+                    "Hello",
+                    style: GoogleFonts.sourceCodePro(
+                      fontSize: 50,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
