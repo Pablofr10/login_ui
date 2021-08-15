b2b7376b4266d12906682f00817fb3ff8e37cfa7
Pablorf10 <rafanet.live@gmail.com>
2021-08-15 19:37:58 -0300

    Formatando e criando LoginSignUpPage widget

---
+++ lib/LoginSignUpPage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginSignUpPage extends StatefulWidget {
+  @override
+  _LoginSignUpPageState createState() => _LoginSignUpPageState();
+}
+
+class _LoginSignUpPageState extends State<LoginSignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      child: Container(
+        child: SingleChildScrollView(
+          child: Column(),
+        ),
+      ),
+    );
+  }
+}
